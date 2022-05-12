1ccd8c0b25a2c2164144b4f92215b354cb19324b
Valentin MOURTIALON <vmourtia@e3r6p10.clusters.42paris.fr>
2022-05-12 12:04:40 +0200

    s, c, i, d done for printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vmourtia <vmourtia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 10:02:37 by vmourtia          #+#    #+#             */
-/*   Updated: 2022/05/10 16:12:08 by vmourtia         ###   ########.fr       */
+/*   Updated: 2022/05/12 12:03:28 by vmourtia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,38 +41,69 @@
 		ft_putstr_fd((char *)arg, 1);
 }*/
 
+/*void	ft_put_unbr(unsigned int n)
+{
+	if (n == INT_MIN)
+		ft_putstr_fd("-2147483648", fd);
+	else
+	{
+		if (n < 0)
+		{
+			n *= -1;
+			ft_putchar_fd('-', fd);
+		}
+		if (n <= 9)
+			ft_putchar_fd(n + '0', fd);
+		else
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+	}
+}*/	
+
 int ft_printf(const char *s, ...)
 {
-	va_list	lst_arg;
+	va_list	args;
 	size_t	s_length;
 	size_t	i;
 
-	va_start(lst_arg, s);
+	va_start(args, s);
 	i = 0;
 	while (s[i])
 	{
 		if (s[i] == '%')
 		{
 			i++;
+			
 			if (s[i] == 'c')
-				ft_putchar_fd(*((char *)va_arg(lst_arg, void *)), 1);
+				//printf("%c", va_arg(args, int));
+				ft_putchar_fd(va_arg(args, int), 1);
 			else if (s[i] == 's')
-				ft_putstr_fd((char *)va_arg(lst_arg, void *), 1);
+				//printf("%s", va_arg(args, char *));
+				ft_putstr_fd(va_arg(args, char *), 1);
+			else if (s[i] == 'i' || s[i] == 'd')
+				ft_putnbr_fd(va_arg(args, int), 1);
+			else if (s[i] == 'u')
+				ft_put_unbr(va_arg(args, unsigned int));
 		}
 		else
+			//printf("%c", s[i]);
 			ft_putchar_fd(s[i], 1);
 		i++;
 	}
-	va_end(lst_arg);
+	va_end(args);
 	s_length = ft_strlen(s);
     return (s_length);
 }
 
 int main(void)
 {
-	int	a;
+	unsigned int	un=3000000000;
+	int				len;
 	
-    a = ft_printf("Character to display : %c!\nString to display : %s!\n", 'A', "AH!");
-	return (a);
+    len = ft_printf("(c) Character to display : %c.\n(s) String to display : %s.\n(d) Decimal to display : %d.\n(i) Int to display : %i.\n(u) Unsigned decimal to display : %u.\n", 'A', "AH!", -10478, 50, un);
+	ft_printf("\nString length : %i.\n", len);
+	return (0);
 }
 
