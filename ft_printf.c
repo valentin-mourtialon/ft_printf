b56ade1a04775da49cd0825782ddc50342294eef
Valentin MOURTIALON <vmourtia@e3r4p8.clusters.42paris.fr>
2022-06-07 11:50:00 +0200

    Simple unit tests passed

--- ft_printf.c
+++ ft_printf.c
@@ -6,43 +6,23 @@
 /*   By: vmourtia <vmourtia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 10:02:37 by vmourtia          #+#    #+#             */
-/*   Updated: 2022/05/12 15:35:13 by vmourtia         ###   ########.fr       */
+/*   Updated: 2022/06/07 11:47:46 by vmourtia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
-#include "libft/libft.h"
+#include "ft_printf.h"
 
-static void	put_unbr(unsigned int n)
+static void	put_unsgn_nbr_base(\
+	char *base,
+	unsigned int base_length,
+	unsigned long n)
 {
-	if (n <= 9)
-		ft_putchar_fd(n + '0', 1);
+	if (n < base_length)
+		ft_putchar_fd(base[n], 1);
 	else
 	{
-		ft_putnbr_fd(n / 10, 1);
-		ft_putnbr_fd(n % 10, 1);
-	}
-}
-
-static void	putnbr_hex(char *base_hexa, int n)
-{
-	if (n == INT_MIN)
-		ft_putstr_fd("-80000000", 1);
-	else
-	{
-		if (n < 0)
-		{
-			n *= -1;
-			ft_putchar_fd('-', 1);
-		}
-		if (n < 16)
-			ft_putchar_fd(base_hexa[n], 1);
-		else
-		{
-			putnbr_hex(base_hexa, n / 16);
-			putnbr_hex(base_hexa, n % 16);
-		}
+		put_unsgn_nbr_base(base, base_length, n / base_length);
+		put_unsgn_nbr_base(base, base_length, n % base_length);
 	}
 }
 
@@ -70,13 +50,14 @@ static char	*gen_hexabase(int lower_mode)
 	return (base_hexa);
 }
 
-static void	putnbr_dec_to_hexa(int lower_mode, va_list args)
+static void	put_unsgn_dec_to_hexa(int lower_mode, va_list args)
 {
-	char	*base;
+	char			*base_hexa;
 
-	base = gen_hexabase(lower_mode);
-	putnbr_hex(base, va_arg(args, int));
-	free(base);
+	base_hexa = gen_hexabase(lower_mode);
+	put_unsgn_nbr_base(\
+		base_hexa, ft_strlen(base_hexa), va_arg(args, unsigned long));
+	free(base_hexa);
 }
 
 static void	check_options(char c, va_list args)
@@ -85,14 +66,16 @@ static void	check_options(char c, va_list args)
 		ft_putchar_fd(va_arg(args, int), 1);
 	else if (c == 's')
 		ft_putstr_fd(va_arg(args, char *), 1);
+	else if (c == 'p')
+		put_unsgn_dec_to_hexa(1, args);
 	else if (c == 'i' || c == 'd')
 		ft_putnbr_fd(va_arg(args, int), 1);
 	else if (c == 'u')
-		put_unbr(va_arg(args, unsigned int));
+		put_unsgn_nbr_base("0123456789", 10, va_arg(args, unsigned int));
 	else if (c == 'x')
-		putnbr_dec_to_hexa(1, args);
+		put_unsgn_dec_to_hexa(1, args);
 	else if (c == 'X')
-		putnbr_dec_to_hexa(0, args);
+		put_unsgn_dec_to_hexa(0, args);
 	else if (c == '%')
 		ft_putchar_fd(c, 1);
 }
@@ -117,12 +100,37 @@ int	ft_printf(const char *s, ...)
 	s_length = ft_strlen(s);
 	return (s_length);
 }
-/*
+
 int main(void)
 {
-	int		len;
+	unsigned int	u1 = 1245;
+	unsigned int	u2 = 37;
+	unsigned int	u3 = 0;
+	int				len;
+	void			*p;
 	
-    len = ft_printf("(c) Character to display : %c.\n(s) String to display : %s.\n(d) Decimal to display : %d.\n(i) Int to display : %i.\n(u) Unsigned decimal to display : %u.\n(x) Hexadecimal lowercase to display : %x.\n(X) Hexadecimal uppercase to display : %X.\n(%%) .\n", 'A', "AH!", INT_MIN, INT_MAX, UINT_MAX, INT_MIN, INT_MAX);
-	ft_printf("\nString length : %i.\n", len);
+	len = ft_printf("\nTests :\n");
+	p = &len;
+
+	ft_printf("\nft_printf character %%c    = %c", 'A');
+	ft_printf("\nft_printf string    %%s    = %s", "AH!");
+	ft_printf("\nft_printf void *ptr %%p    = %p || %p || %p || %p", p, (void *)&u1, (void *)&u2, (void *)&u3);
+	ft_printf("\nft_printf int (dec) %%d    = %d || %d || %d || %d || %d || %d", INT_MIN, INT_MAX, -34, 0, -0, 9);
+	ft_printf("\nft_printf int       %%i    = %i || %i || %i || %i || %i || %i", INT_MIN, INT_MAX, -34, 0, -0, 9);
+	ft_printf("\nft_printf unsigned  %%u    = %u || %u || %u || %u", UINT_MAX, u1, u2, u3);
+	ft_printf("\nft_printf unsigned  %%x    = %x || %x || %x || %x", UINT_MAX, u1, u2, u3);
+	ft_printf("\nft_printf unsigned  %%X    = %X || %X || %X || %X", UINT_MAX, u1, u2, u3);
+	ft_printf("\n\n");
+
+	printf("\nprintf character    %%c       = %c", 'A');
+	printf("\nprintf string       %%s       = %s", "AH!");
+	printf("\nprintf void *ptr    %%p       = %p || %p || %p || %p", p, (void *)&u1, (void *)&u2, (void *)&u3);
+	printf("\nprintf int (dec)    %%d       = %d || %d || %d || %d || %d || %d", INT_MIN, INT_MAX, -34, 0, -0, 9);
+	printf("\nprintf int          %%i       = %i || %i || %i || %i || %i || %i", INT_MIN, INT_MAX, -34, 0, -0, 9);
+	printf("\nprintf unsigned     %%u       = %u || %u || %u || %u", UINT_MAX, u1, u2, u3);
+	printf("\nprintf unsigned     %%x       = %x || %x || %x || %x", UINT_MAX, u1, u2, u3);
+	printf("\nprintf unsigned     %%X       = %X || %X || %X || %X", UINT_MAX, u1, u2, u3);
+	printf("\n\n");
+
 	return (0);
-}*/
+}
---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmourtia <vmourtia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 09:50:41 by vmourtia          #+#    #+#             */
+/*   Updated: 2022/06/07 09:55:08 by vmourtia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int ft_printf(const char *s, ...);
+
+#endif
